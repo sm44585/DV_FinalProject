4292cbb3a106427cff3babae4273c2cb2e83a24a
cc <zangcc0474@utexas.edu>
2015-11-30 20:26:24 -0600

    I made the ETL file for three data sets. Now, in 01 folder, it should include five R scripts. Make sure you guys get all of them

---
+++ 01 Data/Medianzip_Fastfoodmap_df.R
@@ -0,0 +1,14 @@
+require("jsonlite")
+require("RCurl")
+
+# Loads the data from Fast Food table into Fast Food dataframe
+# Change the USER and PASS below to be your UTEid
+fast_food <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from FASTFOODMAPS_LOCATIONS_2007"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_cz4795', PASS='orcl_cz4795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE),))
+summary(fast_food)
+
+# Loads median, mean, and population data into Zip Code dataframe
+
+zip_code <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from MedianZIP"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_cz4795', PASS='orcl_cz4795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE),))
+
+zip_code$MEAN <- as.numeric(levels(zip_code$MEAN))[zip_code$MEAN]
+summary(zip_code)
