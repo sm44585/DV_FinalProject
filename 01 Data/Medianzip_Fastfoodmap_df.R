9fd0fde765a80467129f9203e21e4336e4df7e5a
Spencer Muncey <scmuncey@yahoo.com>
2015-12-02 17:22:40 -0600

    Re run the fast_food_locations ELT file. You had the correct spelling of Jack in the Box but didn't import the reformatted csv file

--- 01 Data/Medianzip_Fastfoodmap_df.R
+++ 01 Data/Medianzip_Fastfoodmap_df.R
@@ -3,6 +3,8 @@ require("RCurl")
 
 # Loads the data from Fast Food table into Fast Food dataframe
 fast_food <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from FASTFOODMAPS_LOCATIONS_2007"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_cz4795', PASS='orcl_cz4795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+#rename ZIP_CODE column to ZIP to match the zip code dataset
+colnames(fast_food)[5] <- "ZIP"
 summary(fast_food)
 
 # Loads median, mean, and population data into Zip Code dataframe
