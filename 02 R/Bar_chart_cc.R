b53b73fcd982f6f8586e1d9e423476ef946dd69a
cc <zangcc0474@utexas.edu>
2015-12-05 11:31:28 -0600

    Changes including red numbers, size of numbers by sales and dollar x-axis.

--- 02 R/Bar_chart_cc.R
+++ 02 R/Bar_chart_cc.R
@@ -4,6 +4,7 @@ require(dplyr)
 require(tidyr)
 require(ggplot2)
 require(reshape2)
+library(scales)
 
 fast_food <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from FASTFOODMAPS_LOCATIONS_2007"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_cz4795', PASS='orcl_cz4795', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE),))
 #View(fast_food)
@@ -23,6 +24,38 @@ bar_chart<-dplyr::right_join(bar_chart, WINDOW_AVG, by="RESTAURANT")
 
 bar_chart<-bar_chart %>% select (STATE,RESTAURANT,variable,value.x,value.y)%>%mutate(Diff_To_Avg = value.x - value.y)
 #Plot Function to generate bar chart with reference line and values
+ggplot(data=bar_chart, aes(x=STATE, y=value.x)) +
+  geom_bar(stat="identity", fill="steelblue")+
+  facet_wrap(~RESTAURANT)+
+  labs(x=paste("State"), y=paste("Sales"))+
+  geom_text(aes(label=value.x, size=value.x), vjust=0, color="BLACK")+
+  scale_size(range=c(3,6)) +
+  scale_y_continuous(labels = dollar)+
+  geom_hline(yintercept=value.y, linetype="dashed", 
+             color = "red", size=2)+
+  coord_flip()+
+  theme_bw()+
+  theme_minimal()+
+  #geom_hline(yintercept=value.y, linetype="dashed", 
+             color = "red", size=2)
+
++
+  layer(data=bar_chart, 
+        mapping=aes(yintercept = value.y), 
+        geom="hline",
+        linetype="dashed",
+        size=2,
+        geom_params=list(colour="red")
+  )+
+  layer(data=bar_chart, 
+        mapping=aes(x=STATE, y=value.x, label=round(Diff_To_Avg, 2)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(colour="black", hjust=-3), 
+        position=position_identity()
+  )
+#"Pizza Hut","Taco Bell","Wendys","Jack in the Box", "Hardees", "Carls Jr", "In-N-Out","KFC"
 ggplot() + 
   coord_cartesian() + 
   scale_x_discrete() +
@@ -35,11 +68,11 @@ ggplot() +
         stat="identity", 
         stat_params=list(), 
         geom="bar",
-        geom_params=list(colour="blue", fill="white"), 
+        geom_params=list(fill="steelblue"), 
         position=position_dodge()
-  ) + coord_flip() +
+  )+ coord_flip()+  
   layer(data=bar_chart, 
-        mapping=aes(x=STATE, y=value.x, label=round(value.x, 2)), 
+        mapping=aes(x=STATE, y=value.x, label=round(value.x,2),size=value.x),
         stat="identity", 
         stat_params=list(), 
         geom="text",
@@ -47,16 +80,41 @@ ggplot() +
         position=position_identity()
   )+
   layer(data=bar_chart, 
-        mapping=aes(yintercept = value.y), 
-        geom="hline",
-        geom_params=list(colour="red")
+         mapping=aes(yintercept = value.y), 
+         geom="hline",
+         linetype="dashed",
+         size=2,
+         geom_params=list(colour="red")
   )+
   layer(data=bar_chart, 
-        mapping=aes(x=STATE, y=value.x, label=round(Diff_To_Avg, 2)), 
+        mapping=aes(x=STATE, y=value.x, label=round(Diff_To_Avg)), 
         stat="identity", 
         stat_params=list(), 
         geom="text",
-        geom_params=list(colour="black", hjust=-3), 
+        geom_params=list(colour="red", hjust=-2), 
         position=position_identity()
   )
-#"Pizza Hut","Taco Bell","Wendys","Jack in the Box", "Hardees", "Carls Jr", "In-N-Out","KFC"
+
+
+
+
+
+
+
+
+
+
+
++
+  layer(data=bar_chart, 
+        mapping=aes(x=STATE, y=value.x, yintercept = value.y, label=round(value.y)), 
+        stat="identity",
+        #stat_params=list(),
+        geom="text",
+        geom_params=list(colour="black", hjust=0)
+  )
+
+
+
+
+
